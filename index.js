d3a9b3bd1bee31c0c96ad8a11a5f6acb4c3544d1
aumartinez <acm_asd@hotmail.com>
2023-11-28 16:54:08 -0600

    Startup ECMA recommended module usage

---
+++ index.js
@@ -0,0 +1,11 @@
+// console.log('Hello world');
+// console.info('Hello info');
+// console.error('Hello error');
+
+// console.log(typeof window);
+// console.log(globalThis);
+
+// Common JS
+const { sum } = require('./cjs/sum')
+
+console.log(sum(1,2))
---
+++ index.mjs
@@ -0,0 +1,5 @@
+import { sum, sub, mult } from './sum.mjs'
+
+console.log(sum(1, 2))
+console.log(sub(3, 2))
+console.log(mult(2, 3))
